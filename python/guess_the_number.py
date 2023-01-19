9a392df64a40f486222fba7e9fbc7be44b38ae35
csonmarz <caleb.shavers@hgs.hiddengeniusproject.org>
2023-01-18 19:44:08 -0800

    first commit

--- python/guess_the_number.py
+++ python/guess_the_number.py
@@ -14,8 +14,37 @@ def guess(x):
     print(f'Yay, congrat. You have guessed the right number {random_number} correctly!')
 
 
-guess(10)
+def computer_guess(x):
+    low = 1 
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f' Is guess too high (H), too low(L) or correct (C)??').lower()
+        # what this does is we are giving the computer a function where it tries to guess the users number and we then ask the user if our guess is too hig, too low, or just right. Its just like before how the user tries to guess the computers number, but now its reversed.
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+        
+    print(' Yay! The computer guessed your number , {guess} , correctly !')
+
+# The while loop gives the computer an idea is the guess was too high or too low. 
+# If too high, the guess will go down by one number
+# If too low, the guess will go up by one number
+# if correct, it will print our the statement above
+
+
+
         
 # what the else statements do is it gives the computer an idea if the guess if going to higher or lower than the random number chosen by the computer.
 # if the user guesses the number right, it will go into a statement that will say you got it right, otherwise it will tell you if you guesses to high or low
+
+guess(10)
+computer_guess(10)
+
+
 
---
+++ python/rock_paper_scissors/ReadMe.md
@@ -0,0 +1,6 @@
+Variables- A variable is a value that can change, depending on conditions or on information passed to the program
+Lists- Lists are used to store multiple items in a single variable. (Ex of a list options = ["rock", "paper", "scissors"])
+Boolean Logic- Boolean logic is also known as Boolean Operators (“Or,” “And,” and “Not")  What it does is it reverse a fucntion and it gives it a more focused and productive result
+While Loops- A while loop is a control flow statement that allows code to be executed repeatedly based on a given Boolean( True or False or newly used in video( not, and , or))
+Conditional (if/else if/ else)- Conditional statements are used to specify a block of code to be executed, if a specified condition is true.
+Optimization (clean code up and add functions)- Code optimization is writing or rewriting code so a program uses the least possible memory
